306a9f9c9ec2e47bf42bee3940d615bc9631914a
Kevin Roark <ker2143@columbia.edu>
2016-05-19 01:54:51 -0400

    shadow

--- js/main.js
+++ js/main.js
@@ -30,8 +30,12 @@
 (function() {
   var scene, camera, renderer;
 
-  var boxesAreMoving = false;
+  var ground, ambientLight, spt, bigBox;
   var boxes = [];
+  var boxesAreMoving = false;
+
+  var sptColor = {r: 255, g: 175, b: 175, goingBlue: true};
+  function sptColorStr() { return 'rgb(' + sptColor.r + ',' + sptColor.g + ',' + sptColor.b + ')'; }
 
   init();
   animate();
@@ -40,18 +44,33 @@
     scene = new THREE.Scene();
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 20000);
-    camera.position.y = 400;
-    camera.position.z = 2000;
+    camera.position.z = 4000;
+
+    ambientLight = new THREE.AmbientLight(0x555555);
+    scene.add(ambientLight);
+
+    ground = createGround();
+    ground.position.set(0, -2000, 0);
+    scene.add(ground);
+
+    bigBox = new THREE.Object3D();
+    scene.add(bigBox);
+
+    spt = createSpotLight();
+    spt.color = new THREE.Color(sptColorStr());
+    spt.position.set(0, 1500, 2200);
+    scene.add(spt);
+    //scene.add(spt.shadowCameraHelper); // add this to see shadow helper
 
     for (var x = -1000; x <= 1000; x += 200) {
-      for (var z = -1500; z <= 500; z += 200) {
-        for (var y = 0; y < 1000; y += 200) {
+      for (var z = -1000; z <= 1000; z += 200) {
+        for (var y = -1000; y < 1000; y += 200) {
           var box = makeBox();
           box.position.set(x, y, z);
           box._vel = new THREE.Vector3(0, 0, 0);
           box._acc = new THREE.Vector3(randomA(), randomA(), randomA());
 
-          scene.add(box);
+          bigBox.add(box);
           boxes.push(box);
         }
       }
@@ -59,6 +78,11 @@
 
     renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(0xfefefe);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    renderer.gammaInput = true;
+    renderer.gammaOutput = true;
+    renderer.antialias = true;
 
     resize();
     window.addEventListener('resize', resize, false);
@@ -67,11 +91,9 @@
 
     setTimeout(function() {
       boxesAreMoving = true;
-    }, 2000);
 
-    setInterval(function() {
-      resetBoxMovements();
-    }, 7500);
+      setInterval(resetBoxMovements, 3000);
+    }, 5000);
   }
 
   function animate() {
@@ -84,6 +106,21 @@
         box._vel.add(box._acc);
       }
     }
+    else if (bigBox) {
+      bigBox.rotation.y += 0.01;
+    }
+
+    if (sptColor.goingBlue) {
+      sptColor.b += 1;
+      sptColor.r -= 1;
+      if (sptColor.r === 175) sptColor.goingBlue = false;
+    }
+    else {
+      sptColor.r += 1;
+      sptColor.b -= 1;
+      if (sptColor.b === 175) sptColor.goingBlue = true;
+    }
+    spt.color = new THREE.Color(sptColorStr());
 
     renderer.render(scene, camera);
   }
@@ -109,8 +146,8 @@
   function makeBox() {
     var geometry = new THREE.BoxGeometry(200, 200, 200);
 
-    var material = new THREE.MeshBasicMaterial({
-      color: randomGrayColor()
+    var material = new THREE.MeshPhongMaterial({
+      color: 0xaaaaaa
     });
 
     var mesh = new THREE.Mesh(geometry, material);
@@ -122,6 +159,40 @@
   function randomA() {
     var a = Math.pow(Math.random(), 3);
     return Math.random() < 0.5 ? a : -a;
+  }
+
+  function createGround() {
+    var geometry = new THREE.PlaneGeometry(10000, 10000);
+    geometry.computeFaceNormals();
+    geometry.computeVertexNormals();
+
+    var material = new THREE.MeshPhongMaterial({
+      color: 0xaaaaaa,
+      emissive: 0xaaaaaa,
+      side: THREE.DoubleSide
+    });
+
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.x = -Math.PI / 2;
+    mesh.receiveShadow = true;
+
+    return mesh;
+  }
+
+  function createSpotLight() {
+    var spt = new THREE.SpotLight(0xffffff, 1.5);
+    spt.castShadow = true;
+    spt.shadow.camera.near = 0.1;
+    spt.shadow.camera.far = 20000;
+    spt.shadow.mapSize.width = spt.shadow.mapSize.height = 1024;
+    spt.shadowCameraHelper = new THREE.CameraHelper(spt.shadow.camera); // colored lines
+    spt.angle = 1.0;
+    spt.exponent = 2.0;
+    spt.penumbra = 0.15;
+    spt.decay = 1.25;
+    spt.distance = 6000;
+
+    return spt;
   }
 
   function randomBrightColor() {
